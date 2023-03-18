41a6f467ed892ca4929b891fec9e43b1333965f2
Bruno Arueira <contato@brunoarueira.com>
2023-03-18 16:38:15 -0300

    Config github actions as CI (#9)

--- backend/healthier/app/__init__.py
+++ backend/healthier/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.apis.status import mod as statusModule
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ app.register_blueprint(statusModule)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug = os.environ["FLASK_ENV"] == "development"
+
+    app.run(debug=debug)
